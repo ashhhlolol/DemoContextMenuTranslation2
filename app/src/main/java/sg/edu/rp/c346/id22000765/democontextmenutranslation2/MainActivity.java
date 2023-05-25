2db323e8e344e97d101a86df634aa96959707269
Ashley <22000765@myrp.edu.sg>
2023-05-25 12:11:49 +0800

    Finished

--- app/src/main/java/sg/edu/rp/c346/id22000765/democontextmenutranslation2/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000765/democontextmenutranslation2/MainActivity.java
@@ -3,12 +3,77 @@ package sg.edu.rp.c346.id22000765.democontextmenutranslation2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvTranslatedText;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedText.setText("Hello ");
+            return true;
+        } else if (id == R.id.italianSelection) {
+            tvTranslatedText.setText("Ciao");
+            return true;
+        } else {
+            tvTranslatedText.setText("Error translation");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+
+        registerForContextMenu(tvTranslatedText);
     }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0, 0, 0, "English");
+        menu.add(0, 1, 1, "Italian");
+
+
+    }
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId()==0) { //check whether the selected menu item ID is 0
+            //code for action
+            Toast.makeText(MainActivity.this, "English is chosen", Toast.LENGTH_SHORT).show();
+            tvTranslatedText.setText("Hello");
+            return true; //menu item successfully handled
+
+        }
+        else if(item.getItemId()==1) { //check if the selected menu item ID is 1
+            //code for action
+            Toast.makeText(MainActivity.this, "Italian is chosen", Toast.LENGTH_SHORT).show();
+            tvTranslatedText.setText("Ciao");
+            return true;  //menu item successfully handled
+        }
+        return super.onContextItemSelected(item); //pass menu item to the superclass implementation
+    }
+
+
 }
